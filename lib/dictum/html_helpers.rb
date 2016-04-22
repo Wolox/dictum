2ac47e0a279d58714a4f23f80e4899940ee90c23
alebian <alebezdjian@gmail.com>
2016-04-22 12:41:22 -0300

    Added HTML helpers tests

--- lib/dictum/html_helpers.rb
+++ lib/dictum/html_helpers.rb
@@ -35,15 +35,18 @@ module Dictum
         "</div>\n</div>"
       end
 
-      def script(text)
-        "<script src='#{text}'></script>"
+      def script(script_path)
+        return '' unless script_path
+        "<script src='#{script_path}'></script>"
       end
 
-      def external_css(text)
-        "<link rel='stylesheet' href='#{text}'>"
+      def external_css(css_path)
+        return '' unless css_path
+        "<link rel='stylesheet' href='#{css_path}'>"
       end
 
       def unordered_list(elements)
+        return "<ul>\n</ul>" unless elements
         answer = "<ul>\n"
         elements.each do |element|
           answer += "<li><a href='#{element.downcase}.html'>#{element}</a></li>\n"
@@ -52,14 +55,22 @@ module Dictum
       end
 
       def title(text, html_class = nil)
+        return "<h1>#{text}</h1>" unless html_class
         "<h1 class='#{html_class}'>#{text}</h1>"
       end
 
       def subtitle(text, html_class = nil)
+        return "<h3>#{text}</h3>" unless html_class
         "<h3 class='#{html_class}'>#{text}</h3>"
       end
 
+      def sub_subtitle(text, html_class = nil)
+        return "<h4>#{text}</h4>" unless html_class
+        "<h4 class='#{html_class}'>#{text}</h4>"
+      end
+
       def paragraph(text, html_class = nil)
+        return "<p>#{text}</p>" unless html_class
         "<p class='#{html_class}'>#{text}</p>"
       end
 
@@ -70,14 +81,13 @@ module Dictum
       end
 
       def code_block(title, json)
+        return '' unless json
+        return code(json) unless title
         "#{sub_subtitle(title)}\n#{code(json)}"
       end
 
-      def sub_subtitle(text, html_class = nil)
-        "<h4 class='#{html_class}'>#{text}</h4>"
-      end
-
       def code(json)
+        return '' unless json
         "<pre class='prettyprint'>#{json}</pre>"
       end
     end
